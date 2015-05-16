88ab1503380fd510bbf12dc5edd73cfb27664352
MarcioPorto <mflporto@gmail.com>
2015-05-16 19:03:21 -0300

    Finished the launcher activity and added a new one.

---
+++ app/src/main/java/com/marcioporto/interactivestory/StoryActivity.java
@@ -0,0 +1,29 @@
+package com.marcioporto.interactivestory;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.marcioporto.interactivestory.R;
+
+public class StoryActivity extends Activity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        if (name == null) {
+            name = "Friend";
+        }
+        Log.d(TAG, name);
+    }
+
+}
